954668b975687c41ddd94e0c547332fcb808439d
Victor Fajardo <fajardo.de.leon@gmail.com>
2025-08-17 00:45:01 -0400

    savepoit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,10 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from 'axios';
+import type { User } from 'oidc-client-ts';
 
 const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 // Generate headers dynamically using the auth user token
-const getHeaders = (authUser?: any) => ({
+const setConfig = (authUser?: User) => ({
   headers: {
     'Content-Type': 'application/json',
     ...(authUser?.access_token ? { Authorization: `Bearer ${authUser.id_token}` } : {}),
@@ -19,26 +19,22 @@ export type Note = {
 };
 
 // Fetch all notes
-export const getNotes = async (authUser?: any): Promise<Note[]> => {
-  const res = await axios.get(API_BASE_URL, { ...getHeaders(authUser), withCredentials: true });
+export const getNotes = async (authUser?: User): Promise<Note[]> => {
+  const res = await axios.get(API_BASE_URL, setConfig(authUser));
   return res.data.notes.sort((a: Note, b: Note) => a.createdAt.localeCompare(b.createdAt));
 };
 
 // Create a new note
-export const createNote = async (content: string, authUser?: any): Promise<void> => {
-  await axios.post(API_BASE_URL, { content }, { ...getHeaders(authUser), withCredentials: true });
+export const createNote = async (content: string, authUser?: User): Promise<void> => {
+  await axios.post(API_BASE_URL, { content }, setConfig(authUser));
 };
 
 // Update a note by ID
-export const updateNote = async (id: string, content: string, authUser?: any): Promise<void> => {
-  await axios.put(
-    `${API_BASE_URL}/${id}`,
-    { content },
-    { ...getHeaders(authUser), withCredentials: true }
-  );
+export const updateNote = async (id: string, content: string, authUser?: User): Promise<void> => {
+  await axios.put(`${API_BASE_URL}/${id}`, { content }, setConfig(authUser));
 };
 
 // Delete a note by ID
-export const deleteNote = async (id: string, authUser?: any): Promise<void> => {
-  await axios.delete(`${API_BASE_URL}/${id}`, { ...getHeaders(authUser), withCredentials: true });
+export const deleteNote = async (id: string, authUser?: User): Promise<void> => {
+  await axios.delete(`${API_BASE_URL}/${id}`, setConfig(authUser));
 };
