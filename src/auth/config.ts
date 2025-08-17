d2c8e8d60c913790f06aa04f6e1525bac71fc67f
Victor Fajardo <fajardo.de.leon@gmail.com>
2025-08-16 20:44:18 -0400

    ⚙ add cognito support

--- src/auth/config.ts
+++ src/auth/config.ts
@@ -1,7 +1,10 @@
 export const cognitoAuthConfig = {
   authority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_ySM950XJd',
   client_id: '3vc8153ff8kdomu3t9fi1hi2cr',
-  redirect_uri: 'http://localhost:5173/lambda-dynamodb-ui/',
+  redirect_uri:
+    import.meta.env.MODE === 'development'
+      ? 'http://localhost:5173/lambda-dynamodb-ui/'
+      : 'https://victorfajardo.github.io/lambda-dynamodb-ui/',
   response_type: 'code',
   scope: 'email openid profile',
 };
